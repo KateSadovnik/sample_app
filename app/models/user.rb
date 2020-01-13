67389551f4ff481af8577930a443eb73fbf7a44b
KateSadovnik <sadovnikovakate91@gmail.com>
2020-01-13 21:58:34 +0300

    FOR new_branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+	validates :name, presence: true, length: { maximum: 2 }
+	VALID_REGEXP = /\A[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+\z/
+	validates :email, presence: true, format: { with: VALID_REGEXP}
 end
