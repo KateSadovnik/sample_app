b15d067ca33dc5498207a029a68bb2be60be4cb1
KateSadovnik <sadovnikovakate91@gmail.com>
2020-01-16 21:47:12 +0300

    index_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ApplicationRecord
-	validates :name, presence: true, length: { maximum: 2 }
+	before_save :to_lower_case
+
+	validates :name, presence: true, length: { maximum: 6 }
 	VALID_REGEXP = /\A[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+\z/
-	validates :email, presence: true, format: { with: VALID_REGEXP}
+	validates :email, presence: true, format: { with: VALID_REGEXP}, 
+						uniqueness: {case_sensetive: false}
+	has_secure_password
+
+	private
+
+	def to_lower_case
+		self.email = email.downcase
+	end
+
 end
