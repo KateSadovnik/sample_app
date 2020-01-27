183af79a6b771603c1f27bd1527133cd7d76c62b
KateSadovnik <sadovnikovakate91@gmail.com>
2020-01-27 22:05:57 +0300

    sessions and remember me and forget me

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+	attr_accessor :remember_token
+
 	before_save :to_lower_case
 
 	validates :name, presence: true, length: { maximum: 6 }
@@ -7,10 +9,41 @@ class User < ApplicationRecord
 						uniqueness: {case_sensetive: false}
 	has_secure_password
 
+# Returns the hash digest of the given string.
+	def self.digest(string)
+		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+		BCrypt::Password.create(string, cost: cost)
+	end
+
+
+# Returns a random token.
+	def self.new_token
+		SecureRandom.urlsafe_base64
+	end
+
+# Remembers a user in the database for use in persistent sessions.
+	def remember
+		self.remember_token = User.new_token
+		update_attribute(:remember_digest, User.digest(remember_token))
+	end
+
+	# Returns true if the given token matches the digest.
+	def authenticated?(remember_token)
+		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	end
+
+# Forgets a user.
+	def forget
+		update_attribute(:remember_digest, nil)
+	end
+
 	private
 
 	def to_lower_case
 		self.email = email.downcase
 	end
+
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   #get 'users/show'
   #get 'users/new'
   resources :users
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
